38f455a072c3d3b4d5cc51bbf3b5e3a702e17718
MOUSSAOUI Mohamed <mohamedmoussaoui@live.fr>
2011-12-02 02:06:11 +0100

    Signed-off-by: MOUSSAOUI Mohamed <mohamedmoussaoui@live.fr>

--- front/include/MenuUser.class.php
+++ front/include/MenuUser.class.php
@@ -2,11 +2,13 @@
 	class MenuUser{
 		function __construct(){
 			?>
+			<div class="menu_User">
 				<ul>
 					<li><a href="mesNotifications.php">Mes Notifications</a></li>
 					<li><a href="monCompte.php">Mon Compte</a></li>
 					<li><a href="mesAnnonces.php">Mes Annonces</a></li>
 				</ul>
+			</div>
 			<?php 
 		}	
 	}
