3b46aa5ec08b1caffdb70a73473bc1e68423f862
unknown <Kelzo@Kelzo-PC.(none)>
2011-12-04 18:58:59 +0100

    Signed-off-by: unknown <Kelzo@Kelzo-PC.(none)>

--- front/page/MonCompte.class.php
+++ front/page/MonCompte.class.php
@@ -22,24 +22,40 @@
 				$qUtilisateur->update($utilisateur);
 			}
 			?>
-				<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+				<form id="adminForm" action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+					<fieldset ><legend>Gérer son compte </legend>
+					<label for="id">Id</label>
 					<input name="id" type="hidden" value="<?php echo $user->id; ?>"/>
-					Id : <?php echo $user->id; ?><br/>
-					Niveau : <?php echo $user->niveauId; ?><br/>
-					Nombre de grain de poivre : <?php echo $user->poivre ?><br/>
-					Date derniere connection : <?php echo $user->dateDerniereConnection; ?><br/>
-					Nom : <input name="nom" value="<?php echo $user->nom; ?>"/><br/>
-					Prenom : <input name="prenom" value="<?php echo $user->prenom; ?>"/><br/>	
-					Pseudo : <input name="pseudo" value="<?php echo $user->pseudo; ?>"/>	<br/>
-					Password : <input name="password" value="<?php echo $user->password; ?>"/>	<br/>
-					Email : <input name="email" value="<?php echo $user->email; ?>"/>	<br/>
-					Adresse : <input name="adresse" value="<?php echo $user->adresse; ?>"/><br/>
-					Cp : <input name="cp" value="<?php echo $user->cp; ?>"/><br/>
-					Ville : <input name="ville" value="<?php echo $user->ville; ?>"/><br/>
-					Telephone Fixe : <input name="telephoneFixe" value="<?php echo $user->telephoneFixe; ?>"/><br/>
-					Telephone Portable : <input name="telephonePortable" value="<?php echo $user->telephonePortable; ?>"/><br/>
-					Lien Photo : <input name="photo" value="<?php echo $user->photo; ?>"/>
-					
+					<span><?php echo $user->id; ?></span>
+					<label for="niveauId">Niveau Utilisateur</label>
+					<span><?php echo $user->niveauId; ?></span>
+					<label for="poivre">Vos grains de poivre</label>
+					<span><?php echo $user->poivre ?></span>
+					<label for="dateDerniereConnection">Derniere connection </label>
+					<span><?php echo $user->dateDerniereConnection; ?></span>
+					<label for="nom">Nom</label>
+					<input name="nom" value="<?php echo $user->nom; ?>"/>
+					<label for="prenom">Prenom</label>
+					<input name="prenom" value="<?php echo $user->prenom; ?>"/>
+					<label for="pseudo">Pseudo</label>	
+					<input name="pseudo" value="<?php echo $user->pseudo; ?>"/>
+					<label for="password">Password</label>
+					<input name="password" value="<?php echo $user->password; ?>"/>
+					<label for="email">Email</label>
+					<input name="email" value="<?php echo $user->email; ?>"/>
+					<label for="adresse">Adresse</label>
+					<input name="adresse" value="<?php echo $user->adresse; ?>"/>
+					<label for="cp">Code Postal</label>
+					<input name="cp" value="<?php echo $user->cp; ?>"/>
+					<label for="ville">Ville</label>
+					<input name="ville" value="<?php echo $user->ville; ?>"/>
+					<label for="telephoneFixe">Téléphone</label>
+					<input name="telephoneFixe" value="<?php echo $user->telephoneFixe; ?>"/>
+					<label for="telephonePortable">Téléphone portable</label>
+					<input name="telephonePortable" value="<?php echo $user->telephonePortable; ?>"/>
+					<label for="photo">Lien Photo</label>
+					<input name="photo" value="<?php echo $user->photo; ?>"/>
+					</fieldset>
 					
 					<input value="Editer" type="submit"/>								
 				</form>
