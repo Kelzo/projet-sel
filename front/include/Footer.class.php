cb5614893ff4521df39b02d9f120f8795c6db3e6
unknown <Administrateur@windows-0ad44c4.(none)>
2011-12-07 23:08:43 +0100

    Signed-off-by: unknown <Administrateur@windows-0ad44c4.(none)>

--- front/include/Footer.class.php
+++ front/include/Footer.class.php
@@ -1,7 +1,7 @@
 <?php
 	class Footer{
 		function __construct(){
-		?>		</div> <!--fin wrapp-->
+		?>		<!-- </div> <!--fin wrapp-->
 				<div id="footer">
 					<p><a href="http://www.rousselguillaume.fr" title="Lien vers mon portfolio">Copyright &copy; 2011 - projet SEL.fr - </a></p>
 				</div>
