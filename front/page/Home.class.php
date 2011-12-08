0a6518a60a2b379ed853abbc61c24e5ce60b6b29
unknown <Guillaume@exod-.(none)>
2011-12-08 21:52:52 +0100

    Signed-off-by: unknown <Guillaume@exod-.(none)>

--- front/page/Home.class.php
+++ front/page/Home.class.php
@@ -17,11 +17,16 @@
 				<ul id="service">
 				Proposition :
 			<?php
+				$o=0;
 				while($blop=mysql_fetch_object($listService)){
 				$annonceId=$blop->id;
 				?>
 					<li><a class="homepageLink" href="consulterAnnonce.php?annonce=<?php echo $annonceId;?>">Consulter</a><?php echo "<span>  ".$blop->titre."</span>  /  ".$blop->desc;?></li>
 					<?php
+					$o++;
+				}
+				if($o==0){
+					//afficé a null
 				}
 				?>
 				</ul>				
