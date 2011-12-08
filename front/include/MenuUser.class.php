f1864293f33cc7d38047c3400b49dffe54b424a9
unknown <seb@lol-d17701a02e0.(none)>
2011-12-08 15:09:19 +0100

    Signed-off-by: unknown <seb@lol-d17701a02e0.(none)>

--- front/include/MenuUser.class.php
+++ front/include/MenuUser.class.php
@@ -1,9 +1,17 @@
 <?php
 	class MenuUser{
 		function __construct(){
+			$qUtilisateur = new QueryUtilisateur();
+			$user = $qUtilisateur->getById($_SESSION['id']);
+			$util=new Util();
 			?>
+	
+			<div id="pseudo_area">
+					<span class="pseudo"><?php echo $user->pseudo; ?></span>
+					<span class="poivre">(Grain(s) de poivre: <?php echo $user->poivre; ?>)</span>
+			</div>
 			<div class="menuTop">
-				<div class="menu_User"> <!--Ce div se ferme dans la fin du Search-->
+					<div class="menu_User">
 					<ul>
 						<li><a href="mesNotifications.php">Mes Notifications</a></li>
 						<li><a href="monCompte.php">Mon Compte</a></li>
